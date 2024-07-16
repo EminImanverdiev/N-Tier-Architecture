b5e7e4ed4bca912f83fe3fe2788f0efae5d08f54
elmin007 <imanverdiyevemin414@gmail.com>
2024-07-16 23:08:41 +0400

    Entities,DataAccess Layers coded.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    //Bir klass bos qalmamalidir.Nedense miras almalidir indi ehtiyac olmuya biler ama zamanla lazim olacag.
+    public class Category:IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
