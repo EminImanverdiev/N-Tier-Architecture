8610bf5f36c92d9c69cc55cbb589831f2a7b2089
elmin007 <imanverdiyevemin414@gmail.com>
2024-07-18 23:14:37 +0400

    Added Order entity and created CategoryService on the project

--- DataAccess/Concrete/EntityFramework/NtierDbContext.cs
+++ DataAccess/Concrete/EntityFramework/NtierDbContext.cs
@@ -8,14 +8,24 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class NtierDbContext:DbContext
+    public class NtierDbContext : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=LAPTOP-S4ROCGFN\SQLEXPRESS01;Database=NtierDb;Trusted_Connection=true");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.UnitPrice)
+                .HasColumnType("decimal(18,2)");
+
+        }
+
         public DbSet<Product>? Products { get; set; }
         public DbSet<Category>? Categories { get; set; }
         public DbSet<Customer>? Customers { get; set; }
+        public DbSet<Order>? Orders { get; set; }
     }
 }
