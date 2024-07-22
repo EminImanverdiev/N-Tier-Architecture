1b6c0cac4dcb174ba904c5d4fb16ebfbe4bcba24
elmin007 <imanverdiyevemin414@gmail.com>
2024-07-22 16:40:42 +0400

    Added Extenstion for icollectionservice.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -4,6 +4,8 @@ using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
 using Business.DependencyResolvers.Autofac;
+using Core.DependencyResolvers;
+using Core.Extensions;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
@@ -41,7 +43,10 @@ namespace WebAPI
                     };
                 });
             //AoC 
-
+            builder.Services.AddDepencyResorvers(new Core.Utilities.IoC.ICoreModule[]
+            {
+                new CoreModule()
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
